076b467b5f3748ab206efeb65cf03876be028c92
bael172 <bael171104@gmail.com>
2024-04-05 16:33:25 +0300

    base

---
+++ calc.js
@@ -0,0 +1,9 @@
+let arr_btn = document.querySelectorAll("button")
+arr_btn.forEach((btn)=>{
+    btn.addEventListener('click',function(e){
+        btn.style.backgroundColor='black'
+        btn.style.color='white'
+        btn.style.animation
+
+    })
+})
